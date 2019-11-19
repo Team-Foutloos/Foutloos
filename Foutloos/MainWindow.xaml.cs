d19e7a0fd2de6fb0020abf140ffc41ffe63a2362
Thijs Notkamp <t.notkamp@gmail.com>
2019-11-19 12:50:15 +0100

    Harde reset

--- Foutloos/MainWindow.xaml.cs
+++ Foutloos/MainWindow.xaml.cs
@@ -13,8 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Project_Foutloos
+namespace Foutloos
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
