fd74956fda44f3399d11cb339f162a5fc3a39c90
WahidZeldenrust <43369237+WahidZeldenrust@users.noreply.github.com>
2019-11-19 12:46:20 +0100

    Juiste overzichtspagina aangemaakt ivm fout.

--- Project_Foutloos/OverzichtsPagina.xaml.cs
+++ Project_Foutloos/OverzichtsPagina.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Project_Foutloos
@@ -17,7 +18,7 @@ namespace Project_Foutloos
     /// <summary>
     /// Interaction logic for OverzichtsPagina.xaml
     /// </summary>
-    public partial class OverzichtsPagina : Window
+    public partial class OverzichtsPagina : Page
     {
         public OverzichtsPagina()
         {
