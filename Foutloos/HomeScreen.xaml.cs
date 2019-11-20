d43875f17c288ad6862932c118f2387e7f612cff
Thijs Notkamp <t.notkamp@gmail.com>
2019-11-20 14:49:47 +0100

    Added a login modal, with overlay on the main page.

--- Foutloos/HomeScreen.xaml.cs
+++ Foutloos/HomeScreen.xaml.cs
@@ -25,12 +25,47 @@ namespace Foutloos
         {
             this.owner = owner;
             InitializeComponent();
-            
+
         }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             owner.Content = new ExercisesPage();
+        }
+
+        //This function handles the clicking of the 'login' button.
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            UIElement rootVisual = this.Content as UIElement;
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(rootVisual);
+            if (rootVisual != null && adornerLayer != null)
+            {
+                DarkenAdorner darkenAdorner = new DarkenAdorner(rootVisual);
+                adornerLayer.Add(darkenAdorner);
+                ModalLogin modal = new ModalLogin { Owner = this.owner };
+                modal.ShowDialog();
+                adornerLayer.Remove(darkenAdorner);
+            }
+        }
+    }
+
+    //Create this class to give the 'Darken effect' used while your inside of the modal.
+    public class DarkenAdorner : Adorner
+    {
+        public Brush DarkenBrush { get; set; }
+
+        public DarkenAdorner(UIElement adornedElement)
+          : base(adornedElement)
+        {
+            Brush darkenBrush = new SolidColorBrush(new Color() { R = 0, G = 0, B = 0, A = 140 });
+            darkenBrush.Freeze();
+            DarkenBrush = darkenBrush;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            drawingContext.DrawRectangle(DarkenBrush, null, new Rect(0, 0, AdornedElement.RenderSize.Width, AdornedElement.RenderSize.Height));
         }
     }
 }
