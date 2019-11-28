12b592dec94bbeb774323bf886fd6f36d5f6b7b6
Bart <43670573+BartHijmering@users.noreply.github.com>
2019-11-28 13:04:10 +0100

    log files

---
+++ Foutloos/Results.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Foutloos
+{
+    /// <summary>
+    /// Interaction logic for Results.xaml
+    /// </summary>
+    public partial class Results : Page
+    {
+        public Results()
+        {
+            InitializeComponent();
+            showColumnChart();
+        }
+        private void showColumnChart()
+        {
+           
+            List<KeyValuePair<string, int>> valueList = new List<KeyValuePair<string, int>>();
+            valueList.Add(new KeyValuePair<string, int>("You", 0));
+            valueList.Add(new KeyValuePair<string, int>("Top", 0));
+            valueList.Add(new KeyValuePair<string, int>("Average", 0));
+
+            columnChart.DataContext = valueList;
+        }
+
+        
+    }
+}
