d50371855981358b8ab353170d200ad5a345ecaa
Thijs Notkamp <t.notkamp@gmail.com>
2019-12-17 16:55:31 +0100

    fixed score

--- Foutloos/Multiplayer/ScoreboardScreen.xaml.cs
+++ Foutloos/Multiplayer/ScoreboardScreen.xaml.cs
@@ -133,6 +133,7 @@ namespace Foutloos.Multiplayer
         //When the user clicks the leave button.
         private void ThemedIconButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
+            countdownStoryboard.Stop(this);
             c.leaveRoom();
             Application.Current.MainWindow.Content = new tokenScreen();
         }
