dd699b71fe2c20152ceeccfece01042ebcef9ba9
Floris-Jan <S1130315@windesheim.nl>
2020-01-05 14:33:43 +0100

    Added an error message to the genearator when the user didn't selected one of the options given

--- Foutloos/ExercisesPage.xaml.cs
+++ Foutloos/ExercisesPage.xaml.cs
@@ -512,7 +512,7 @@ namespace Foutloos
         }
 
 
-        //Cecked als er geen letters in txtAmount zit.
+        //Cecks if there are letters in the textbox.
         bool IsDigitsOnly(string str)
         {
             foreach (char c in str)
@@ -524,6 +524,7 @@ namespace Foutloos
             return true;
         }
 
+        //The functionality for the start generated excersie button
         private void StartGeneratedExercise_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             int limit = 75;
@@ -566,6 +567,11 @@ namespace Foutloos
                         startupRandomText(300, true);
                         break;
                 }
+            }
+            else
+            {
+                errorMsg = "Please select one of the options above";
+                lblError.Content = errorMsg;
             }
         }
 
