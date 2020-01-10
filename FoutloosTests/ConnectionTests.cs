3b75447dcc13ad76e432a240d7852387cd801d1d
Thijs Notkamp <t.notkamp@gmail.com>
2020-01-10 12:09:39 +0100

    added tests for connection

---
+++ FoutloosTests/ConnectionTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Foutloos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foutloos.Tests
+{
+    [TestClass()]
+    public class ConnectionTests
+    {
+        [TestMethod()]
+        public void PullDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getPackagesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getPasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void insertIntoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IDTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getPackageCountTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getPackageCountTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getPackageCountFinishedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getPackageNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void leaveRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void checkConnectionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
