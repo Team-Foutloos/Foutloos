07bd58d104a9a5f446ad3f0d6bcfc987ebdda6c8
Thijs Notkamp <t.notkamp@gmail.com>
2020-01-10 12:05:59 +0100

    added exercisespage test

---
+++ FoutloosTests/ExercisesPageTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Foutloos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foutloos.Tests
+{
+    [TestClass()]
+    public class ExercisesPageTests
+    {
+        [TestMethod()]
+        public void ExercisesPageTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
